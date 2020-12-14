edd82d0fe1d99b2b147681c3398e42c61c62fc65
martinsplitt <martin@geekonaut.de>
2020-12-14 13:41:46 +0100

    Nitrox mix calculator is now a PWA

---
+++ static/nitrox/sw.js
@@ -0,0 +1,10 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+workbox.setConfig({
+  debug: true,
+});
+
+workbox.routing.registerRoute(new RegExp('.+\\.(ico|json|png)$'), new workbox.strategies.StaleWhileRevalidate());
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+workbox.precaching.precacheAndRoute(['/nitrox/', '/nitrox', 'index.html']);
