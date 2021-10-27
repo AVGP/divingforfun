9485e4e55c09d167770d2854df13eaa180a95ff1
Martin Splitt <martin@geekonaut.de>
2021-10-27 21:37:21 +0200

    Improves decoplan with validations and small features

--- static/decoplan/decoplanner.js
+++ static/decoplan/decoplanner.js
@@ -1,3 +1,21 @@
+const COMPARTMENTS_ZHL16B = [
+    {halftime:   4.0, a: 1.2600, b: 0.5050},
+    {halftime:   8.0, a: 1.0001, b: 0.6514},
+    {halftime:  12.5, a: 0.8618, b: 0.7222},
+    {halftime:  18.5, a: 0.7563, b: 0.7725},
+    {halftime:  27.0, a: 0.6667, b: 0.8125},
+    {halftime:  38.3, a: 0.5600, b: 0.8434},
+    {halftime:  54.3, a: 0.4947, b: 0.8693},
+    {halftime:  77.0, a: 0.4500, b: 0.8910},
+    {halftime: 109.0, a: 0.4188, b: 0.9092},
+    {halftime: 146.0, a: 0.3799, b: 0.9222},
+    {halftime: 187.0 ,a: 0.3498, b: 0.9319},
+    {halftime: 239.0, a: 0.3223, b: 0.9403},
+    {halftime: 305.0, a: 0.2850, b: 0.9477},
+    {halftime: 390.0, a: 0.2738, b: 0.9544},
+    {halftime: 498.0, a: 0.2524, b: 0.9602},
+    {halftime: 635.0, a: 0.2327, b: 0.9653}
+];
 const COMPARTMENTS_ZHL16C = [
     {halftime:   4.0, a: 1.2600, b: 0.5050},
     {halftime:   8.0, a: 1.0001, b: 0.6514},
@@ -17,6 +35,7 @@ const COMPARTMENTS_ZHL16C = [
     {halftime: 635.0, a: 0.2327, b: 0.9653}
 ];
 const NUM_COMPARTMENTS = 16; // ZHL16C uses 16 compartments.
+const PN2_SURF = 0.79; // in bar. Surface saturation of N2 in compartments
 const LN2 = 0.6931; // ln(2) used in calculating the halftime constant for each compartment
 const pH2O = 0.062; // in bar. See https://www.dekostop.ch/tauchen-know-how/tauchen-dekompression/248-buehlman-zh-l16-inspiratorischer-und-alveolarer-inertgasdruck
 // Schreiner constants we're not using for now.
@@ -49,23 +68,23 @@ function createCompartments(numCompartments, n2Fraction) {
     return new Array(numCompartments).fill(n2Fraction);
 }
 
-function loadCompartmentsAtConstantDepth(compartments, depth, n2Fraction, time) {
+function loadCompartmentsAtConstantDepth(compartments, depth, n2Fraction, time, buehlmannTable = COMPARTMENTS_ZHL16C) {
     // Using the "instantaneous" Bühlmann equation
     for(let c=0; c<NUM_COMPARTMENTS; c++) {
         const p_0 = compartments[c];
         const p_io = getInspiredPressureAtDepth(depth, n2Fraction);
-        compartments[c] = p_0 + (p_io - p_0) * (1 - Math.pow(2, (-time/COMPARTMENTS_ZHL16C[c].halftime)));
+        compartments[c] = p_0 + (p_io - p_0) * (1 - Math.pow(2, (-time/buehlmannTable[c].halftime)));
     }
     return compartments;   
 }
 
-function loadCompartmentsDuringDepthChange(compartments, n2Fraction, startDepth, endDepth, changeRate) {
+function loadCompartmentsDuringDepthChange(compartments, n2Fraction, startDepth, endDepth, changeRate, buehlmannTable = COMPARTMENTS_ZHL16C) {
     const delta_t = (endDepth - startDepth) / changeRate;
     // Using the Schreiner equation
     for(let i=0; i<NUM_COMPARTMENTS; i++) {
         const p_0 = compartments[i];
         const p_io = getInspiredPressureAtDepth(startDepth, n2Fraction);
-        const k = LN2 / COMPARTMENTS_ZHL16C[i].halftime;
+        const k = LN2 / buehlmannTable[i].halftime;
         const c = changeRate * n2Fraction;
         compartments[i] = p_io + c * (delta_t - 1/k) - (p_io - p_0 - (c/k)) * (Math.exp(-k * delta_t)); // Schreiner Equation
     }
@@ -73,10 +92,10 @@ function loadCompartmentsDuringDepthChange(compartments, n2Fraction, startDepth,
 }
 
 // returns ascent ceiling depth in next bigger 3m step
-function getAscentCeilingDepth(compartments, gf) {
+function getAscentCeilingDepth(compartments, gf, buehlmannTable = COMPARTMENTS_ZHL16C) {
     let deepestCeilingDepth = 0;
     for(let c=0;c<NUM_COMPARTMENTS; c++) {
-        const ceilingPressure = (compartments[c] - COMPARTMENTS_ZHL16C[c].a * gf) / ((gf / COMPARTMENTS_ZHL16C[c].b) - gf + 1);
+        const ceilingPressure = (compartments[c] - buehlmannTable[c].a * gf) / ((gf / buehlmannTable[c].b) - gf + 1);
         const ceilingDepth = pressureToDepth(ceilingPressure);
         if(ceilingDepth > deepestCeilingDepth) {
             deepestCeilingDepth = ceilingDepth;
@@ -86,25 +105,25 @@ function getAscentCeilingDepth(compartments, gf) {
 }
 
 // returns a list of sorted deco stops, deepest to shallowest, starting with the bottom time
-function getDivePlan(depth, bottomTime, n2FractionBottom, n2FractionDeco, descentRate, ascentRate, maxPO2Deco, gfLo, gfHi) {
+function getDivePlan(depth, bottomTime, n2FractionBottom, n2FractionDeco, descentRate, ascentRate, maxPO2Deco, gfLo, gfHi, buehlmannTable = COMPARTMENTS_ZHL16C) {
     const plan = [];
     let currentRunTime = bottomTime;
     const bottomMixO2 = Math.round((1 - n2FractionBottom) * 100);
     const decoMixO2 = Math.round((1 - n2FractionDeco) * 100);
     // add bottom time as first row in the dive plan
     plan.push({depth: depth, stopTime: bottomTime, runTime: bottomTime, mix: (bottomMixO2 == 21 ? 'Air' : 'EAN' + bottomMixO2)});
-    let compartments = createCompartments(NUM_COMPARTMENTS, 0.79); // surface N2 pressure
+    let compartments = createCompartments(NUM_COMPARTMENTS, PN2_SURF); // surface N2 pressure
     // descent
-    compartments = loadCompartmentsDuringDepthChange(compartments, n2FractionBottom, 0, depth, descentRate);
+    compartments = loadCompartmentsDuringDepthChange(compartments, n2FractionBottom, 0, depth, descentRate, buehlmannTable);
     const descentTime = Math.ceil(depth / descentRate);
     // bottom time
     const actualBottomTime = bottomTime - descentTime;
-    compartments = loadCompartmentsAtConstantDepth(compartments, depth, n2FractionBottom, actualBottomTime);
+    compartments = loadCompartmentsAtConstantDepth(compartments, depth, n2FractionBottom, actualBottomTime, buehlmannTable);
 
     let currentDepth = depth;
     // get stop depth
     // Note: I'm not sure that using gfLo is the right thing here, but it makes sense as gfLo influences the first stop depth
-    let currentCeilingDepth = getAscentCeilingDepth(compartments, gfLo);
+    let currentCeilingDepth = getAscentCeilingDepth(compartments, gfLo, buehlmannTable);
     // calculate gradient factor slope from first stop
     const deltaGf = (gfHi - gfLo) / -currentCeilingDepth;
 
@@ -117,9 +136,9 @@ function getDivePlan(depth, bottomTime, n2FractionBottom, n2FractionDeco, descen
         // off-gas until we can go to the next stop
         do {
             const n2frac = isDecoMixBreathable(decoMixO2/100, currentCeilingDepth, maxPO2Deco) ? n2FractionDeco : n2FractionBottom;
-            compartments = loadCompartmentsAtConstantDepth(compartments, currentCeilingDepth, n2frac, 1);
+            compartments = loadCompartmentsAtConstantDepth(compartments, currentCeilingDepth, n2frac, 1, buehlmannTable);
             const gf = deltaGf * currentCeilingDepth + gfHi;
-            nextCeilingDepth = getAscentCeilingDepth(compartments, gf);
+            nextCeilingDepth = getAscentCeilingDepth(compartments, gf, buehlmannTable);
             stopTime++;
         } while(nextCeilingDepth === currentCeilingDepth && nextCeilingDepth > 0);
         currentRunTime += stopTime;
@@ -140,11 +159,13 @@ function getOptimalBottomMixForDepth(depth, maxPPO2) {
 
 function getOptimalDecoMixFor(depth, time, n2Fraction, descentRate, ascentRate, maxPPO2, gfLo, gfHi) {
     const plan = getDivePlan(depth, time, n2Fraction, n2Fraction, descentRate, ascentRate, maxPPO2, gfLo, gfHi);
+    if(plan.length === 1) return 1; // bail out if there are no deco stops. 100% O2 is always great at the surface ;-)
     const deepestStopDepth = plan[1].depth; // plan[0] is the bottom time, plan[1] is the first deco stop
     return maxPPO2 / depthToPressure(deepestStopDepth);    
 }
 
-function calculateNoStopTime(depth, n2_frac) {
+// This produces slightly too long values
+function calculateNoStopTimeOld(depth, n2_frac, buehlmannTable = COMPARTMENTS_ZHL16C) {
     /*
     t = -T * log2(p_i - p_t / p_i - p_0)
     t = remaining time in minutes
@@ -153,27 +174,64 @@ function calculateNoStopTime(depth, n2_frac) {
     p_t = tolerated inert gas pressure in bar
     p_0 = inert gas pressure at the beginning of the exposure in bar
     */
-    const p_0 = n2_frac;
-    const surfToleratedPressures = COMPARTMENTS_ZHL16C.map((compartment) => getToleratedPressureForCompartmentAtDepth(0, compartment));
-    let compartments = createCompartments(NUM_COMPARTMENTS, n2_frac);
+//    const p_0 = n2_frac;
+//    const surfToleratedPressures = COMPARTMENTS_ZHL16C.map((compartment) => getToleratedPressureForCompartmentAtDepth(0, compartment));
+    let compartments = createCompartments(NUM_COMPARTMENTS, PN2_SURF);
     // we increase dive time minute by minute and check if there is a compartment that is too oversaturated to allow surfacing.
     let noStopTime = 0;
     do {
         noStopTime++;
-        loadCompartmentsAtConstantDepth(compartments, depth, n2_frac, 1);
+        loadCompartmentsAtConstantDepth(compartments, depth, n2_frac, 1, buehlmannTable = COMPARTMENTS_ZHL16C);
         // as long as all compartments have less supersaturation than is safe to surface with, we can continue the dive      
-    } while(compartments.every((compartment, num) => surfToleratedPressures[num] >= compartment));
+    } while(getAscentCeilingDepth(compartments, 1.0, buehlmannTable) < 3); //compartments.every((compartment, num) => surfToleratedPressures[num] >= compartment));
     return noStopTime;
+}
+
+function calculateNoStopTime(depth, n2Fraction, descentRate, ascentRate, gfLo, gfHi, buehlmannTable = COMPARTMENTS_ZHL16C) {
+    let time = 0;
+    let stops = 0;
+    do {
+        time++;
+        stops = getDivePlan(depth, time, n2Fraction, n2Fraction, descentRate, ascentRate, 1, gfLo, gfHi, buehlmannTable).length;
+    } while(stops < 2 && time < 1000);
+    return time - 1; // we know that the current value of time produces a deco stop, so the no stop time is one minute shorter.
+}
+
+// returns an error message to display or false if there is none.
+function getSettingErrors(depth, bottomTime, o2FractionBottom, o2FractionDeco, maxPO2Bottom, gfLo, gfHi) {
+    // is bottom mix within limits for the depth?
+    const isDepthOk = (!isNaN(depth)) && (depth > 0);
+    const isTimeOk = (!isNaN(bottomTime)) && (bottomTime > 0);
+    const isBottomMixOk = Math.round(o2FractionBottom * 100) <= Math.round(getOptimalBottomMixForDepth(depth, maxPO2Bottom) * 100);
+    const isDecoMixOK = o2FractionBottom <= o2FractionDeco;
+    const areGFsOk = gfLo <= gfHi;
+
+    if(!isDepthOk) {
+        return 'Dive depth should at least be 1 meter.';
+    } else if(!isTimeOk) {
+        return 'Dive time should at least be 1 minute.';
+    }else if(!isBottomMixOk) {
+        return 'Bottom gas exceeds maximum ppO2.';
+    } else if(!isDecoMixOK) {
+        return 'Deco gas should not have less O2% than bottom gas.';
+    } else if(!areGFsOk) {
+        return 'GF Low needs to be lower or equal to GF high.';
+    } else {
+        return false;
+    };
 }
 
 // UI event handlers
 
+let currentBuehlmannTable = COMPARTMENTS_ZHL16C;
+
 function planDive() {
-    const n2FractionBottom = 1.0 - (parseFloat(document.getElementById('bottom_o2').value) / 100);
-    const n2FractionDeco = 1.0 - (parseFloat(document.getElementById('deco_o2').value) / 100);
+    const o2FractionBottom = (parseFloat(document.getElementById('bottom_o2').value) / 100);
+    const o2FractionDeco = (parseFloat(document.getElementById('deco_o2').value) / 100);
+    const n2FractionBottom = 1.0 - o2FractionBottom;
+    const n2FractionDeco = 1.0 - o2FractionDeco;
     const depth = parseFloat(document.getElementById('depth').value);
     const time = parseFloat(document.getElementById('bottom_time').value);
-    const noStopTime = calculateNoStopTime(depth, n2FractionBottom);
     const descentRate = parseFloat(document.getElementById('descent_rate').value);
     const ascentRate = parseFloat(document.getElementById('ascent_rate').value);
     const maxPO2Bottom = parseFloat(document.getElementById('max_ppo2_bottom').value);
@@ -181,8 +239,23 @@ function planDive() {
     const gfLo = parseFloat(document.getElementById('gf_lo').value) / 100;
     const gfHi = parseFloat(document.getElementById('gf_hi').value) / 100;
 
+    const resultsSection = document.getElementById('results');
+    const errorContainer = document.getElementById('error');
+    resultsSection.classList.add('hidden');
+    errorContainer.classList.add('hidden');
+
+    const errMsg = getSettingErrors(depth, time, o2FractionBottom, o2FractionDeco, maxPO2Bottom, gfLo, gfHi);
+    if(errMsg) {
+        errorContainer.textContent = errMsg;
+        errorContainer.classList.remove('hidden');
+        return;
+    }
+
+    // get no stop time...
+    const noStopTime = calculateNoStopTime(depth, n2FractionBottom, descentRate, ascentRate, gfLo, gfHi);
+
     // get dive plan...
-    const plan = getDivePlan(depth, time, n2FractionBottom, n2FractionDeco, descentRate, ascentRate, maxPO2Deco, gfLo, gfHi);
+    const plan = getDivePlan(depth, time, n2FractionBottom, n2FractionDeco, descentRate, ascentRate, maxPO2Deco, gfLo, gfHi, currentBuehlmannTable);
     const tableBody = document.getElementById('diveplan');
     tableBody.innerHTML = '';
 
@@ -202,15 +275,54 @@ function planDive() {
     document.getElementById('optimal_deco_ean').textContent = 'EAN' + Math.round(getOptimalDecoMixFor(depth, time, n2FractionBottom, descentRate, ascentRate, maxPO2Deco, gfLo, gfHi) * 100);
     document.getElementById('no_stop').textContent = noStopTime + ' min';
     document.getElementById('total_stop_time').textContent = totalStopTime + ' min';
+    resultsSection.classList.remove('hidden');
 }
 
 function getNoStopTime() {
-    const n2Fraction = 1.0 - (parseFloat(document.getElementById('bottom_o2').value) / 100);
+    const o2FractionBottom = (parseFloat(document.getElementById('bottom_o2').value) / 100);
+    const n2Fraction = 1.0 - o2FractionBottom;
     const depth = parseFloat(document.getElementById('depth').value);
-    const noStopTime = calculateNoStopTime(depth, n2Fraction);
+    const descentRate = parseFloat(document.getElementById('descent_rate').value);
+    const ascentRate = parseFloat(document.getElementById('ascent_rate').value);
+    const gfLo = parseFloat(document.getElementById('gf_lo').value) / 100;
+    const gfHi = parseFloat(document.getElementById('gf_hi').value) / 100;
+    const maxPO2Bottom = parseFloat(document.getElementById('max_ppo2_bottom').value);
+
+    const resultsSection = document.getElementById('results');
+    const errorContainer = document.getElementById('error');
+    errorContainer.classList.add('hidden');
+    resultsSection.classList.add('hidden');
+
+
+    const errMsg = getSettingErrors(depth, 1, o2FractionBottom, o2FractionBottom, maxPO2Bottom, gfLo, gfHi);
+    if(errMsg) {
+        errorContainer.textContent = errMsg;
+        errorContainer.classList.remove('hidden');
+        document.getElementById('bottom_time').value = 0;
+        return;
+    }
+
+    const noStopTime = calculateNoStopTime(depth, n2Fraction, descentRate, ascentRate, gfLo, gfHi, currentBuehlmannTable);
 
     document.getElementById('bottom_time').value = noStopTime;
 }
 
+function getBestMix() {
+    const depth = parseFloat(document.getElementById('depth').value);
+    const maxPO2Bottom = parseFloat(document.getElementById('max_ppo2_bottom').value);
+    const bottomO2Elem = document.getElementById('bottom_o2');
+    bottomO2Elem.value = Math.round(getOptimalBottomMixForDepth(depth, maxPO2Bottom) * 100);
+}
+
+function changeAlgorithm() {
+    if(this.value === 'B') {
+        currentBuehlmannTable = COMPARTMENTS_ZHL16B;
+    } else {
+        currentBuehlmannTable = COMPARTMENTS_ZHL16C;
+    }
+}
+
 document.getElementById('plan').addEventListener('click', planDive);
 document.getElementById('nostop').addEventListener('click', getNoStopTime);
+document.getElementById('best_mix').addEventListener('click', getBestMix);
+document.getElementById('table').addEventListener('change', changeAlgorithm);
