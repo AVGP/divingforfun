8d06a02f06409d09544b82cfcfd84ec7d0463f7f
Martin Splitt <martin@geekonaut.de>
2021-11-11 23:52:41 +0100

    Fixing SW for nitrox calc

--- static/nitrox/sw.js
+++ static/nitrox/sw.js
@@ -5,6 +5,7 @@ workbox.setConfig({
 });
 
 workbox.routing.registerRoute(new RegExp('.+\\.(ico|json|png)$'), new workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(new RegExp('/nitrox/?$'), new workbox.strategies.StaleWhileRevalidate());
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
-workbox.precaching.precacheAndRoute(['/nitrox/', '/nitrox', 'index.html']);
+
